92ecbec52731b494e02f0b61dd7da64f9f479141
mycahdt <79180400+mycahdt@users.noreply.github.com>
2023-08-03 15:05:55 -0400

    Add files via upload

---
+++ ps.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+
+
+//Program to test the invocation of crsp()
+int
+main(void)
+{
+  printf(1, "name   pid   state\n");
+  printf(1, "--------------------\n");
+  crsp();
+  exit();
+  return 0;
+}
+
---
+++ sysproc.c
@@ -0,0 +1,120 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "spinlock.h"
+
+//Mycah edit: added the external variable to access ptable
+extern struct {
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable;
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+// Mycah Edit:
+// sys_crsp is a function that prints out the current running and sleeping processes
+int
+sys_crsp(void)
+{
+  struct proc *p;
+
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state == RUNNING){
+        cprintf("%s    %d   RUNNING\n", p->name, p->pid);
+      } else if (p->state == SLEEPING) {
+        cprintf("%s    %d   SLEEPING\n", p->name, p->pid);
+      } else if (p->state == RUNNABLE) {
+        cprintf("%s    %d   RUNNABLE\n", p->name, p->pid);
+      }
+    }
+    release(&ptable.lock);
+  return 0;
+};
